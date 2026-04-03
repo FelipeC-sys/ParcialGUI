bbbc6f834cc421dc236fb641dee0776d4da81fe3
Andres Felipe Gallego Carvajal <9felipegc@gmail.com>
2026-04-03 12:54:18 -0500

    Change database connection details in ConexionBD
    Updated the database connection URL and port for ConexionBD.

--- src/dao/ConexionBD.java
+++ src/dao/ConexionBD.java
@@ -8,10 +8,9 @@ public class ConexionBD {
     public static Connection getConnection() {
         try {
             return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/tiendaelectro",
-                "root",
-                ""
-            );
+                    "jdbc:mysql://localhost:3308/TiendaElect",
+                    "root",
+                    "");
         } catch (Exception e) {
             System.out.println("Error conexión: " + e);
             return null;
